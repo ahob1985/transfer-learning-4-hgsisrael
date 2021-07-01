c762e8734726cc76b44b05d3e8fb60e45c1593ee
Israel Fleetwood <67342332+hgsisrael@users.noreply.github.com>
2021-07-01 20:27:20 +0000

    finished

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ let addedExamples;
 //let featureExtractor;
 
 function setup() {
-  canvasDiv = createDiv() {
+  canvasDiv = createDiv(); 
   canvas = createCanvas(640, 480);
   canvas.parent(canvasDiv);
   textDiv = createDiv();
@@ -41,21 +41,27 @@ function setup() {
 }
 
 function draw() {
-
+  if(isModelReady) {
+    image(video, 0, 0);
+  }
+  if(isTrainingComplete) {
+    predictor.predict(canvas, gotResults);
+  }
 }
 
 function buildInput() {
   sliderDiv = createDiv();
   sadSpan = createSpan("Sad");
+  sadSpan.parent(sliderDiv);
   slider = createSlider(0, 1, 0.5, 0,01);
   slider.parent(sliderDiv);
   happySpan = createSpan("Happy");
   happySpan.parent(sliderDiv);
-
+  
   buttonDiv = createDiv();
   addExampleButton = createButton("Add Example");
   addExampleButton.parent(buttonDiv);
-  addExampleButton.mousepressed(function() {
+  addExampleButton.mousePressed(function() {
     addedExamples++;
     textP.html("Added Examples: " + addedExamples);
     predictor.addImage(canvas, slider.value());
@@ -66,25 +72,41 @@ function buildInput() {
     buttonDiv.style("display", "none");
     sliderDiv.style("display", "none");
     predictor.train(whileTraining);
-  })
+  });
+  buttonDiv.style("display", "none");
+  sliderDiv.style("display", "none");
 }
 
 function videoReady() {
+  video.style("display", "none");
+  featureExtractor =  ml5.featureExtractor("MobileNet", featureExtractorLoaded);
 
 }
 
 function featureExtractorLoaded() {
-
+  predictor = featureExtractor.regression(canvas, modelReady);
 }
 
 function modelReady() {
-
+  isModelReady = true;
+  textP.html("Begin adding examples to training data!");
+  sliderDiv.style("display", "block");
+  buttonDiv.style("display", "block");
 }
 
 function whileTraining(loss) {
-
+  if(loss) {
+    console.log(loss);
+  }else {
+    isTrainingComplete = true;
+  }
 }
 
 function gotResults(error, results) {
-
+  if(error) {
+    console.error(error);
+  }else {
+    let value = floor(results.value * 100);
+    textP.html("Happiness: " + value + "%");
+  }
 }
